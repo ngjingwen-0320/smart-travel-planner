e40ad1a6b03ee2825792b81a361458c400406fc0
jiaxuan96 <p23014859@student.newinti.edu.my>
2026-05-02 01:59:40 +0800

    Add password toggle

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -83,6 +83,22 @@ if (loginForm) {
     });
 }
 
+// --- SHOW & HIDE PASSWORD ---
+function togglePassword(password, eyeIcon) {
+    const input = document.getElementById(password);
+    const icon = document.getElementById(eyeIcon);
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
+
 // --- LOGOUT LOGIC ---
 function logout() {
     // 1. Ask for confirmation
